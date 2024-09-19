12a01b891cd0cc56713019ad798e4d3ffb725352
Jack Taylor <jackjta@outlook.com.au>
2024-09-19 17:28:21 +0200

    docs: add comments to main.py

--- exsearch/main.py
+++ exsearch/main.py
@@ -8,15 +8,16 @@ from searcher import Searcher
 
 
 def main():
-    filterwarnings("ignore", category=UserWarning, module="openpyxl")
+    filterwarnings("ignore", category=UserWarning, module="openpyxl")  # This disables irrelevant
+    # warnings.
 
     query = Query()
-    query.get_query_parameters()
+    query.get_query_parameters()  #  This uses the previously created query and initialises values.
 
     searcher = Searcher()
-    searcher.run_query(query)
+    searcher.run_query(query)  # This runs the passed-in query across a path.
 
-    input("\nPress enter to exit.")
+    input("Press enter to exit.")  # This keeps the window open until the user is ready to exit.
 
 
 if __name__ == "__main__":
